e125e9bd94884b29c95dfb349369a26f78ae6d6c
RachelY <yang.yijing@outlook.com>
2023-11-13 22:27:51 -0500

    user can buy scissors

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 //Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
 
 const tools = [
-    {name: 'teeth', generates: 1, price: 0 }
-    {name: 'scissors', generates: 5, price: 5}
-    {name: 'old lawnmower', generates: 50, price: 25}
-    {name: 'new lawnmower', generates: 100, price: 250}
-    {name: 'students', generates: 250, price: 500}
+    {name: 'teeth' , generates: 1, price: 0 },
+    {name: 'scissors', generates: 5, price: 5 },
+    {name: 'old lawnmower', generates: 50, price: 25 },
+    {name: 'new lawnmower', generates: 100, price: 250 },
+    {name: 'students', generates: 250, price: 500 }
 ]
 
 const player = {
@@ -23,7 +23,26 @@ function mowLawn (){
 function upgrade() {
     if (player.tool + 1 < tools.length){
         const nextTool = tools[player.tool + 1]
+        if (nextTool.price < player.money) {
+            money -= nextTool.price
+            player.tool += 1
+        } else {
+            alert(`Money is not enough for this upgrade.`)
+        }
     } else {
-        alert(`You've hired the team and there are no more upgrade.`)
+        alert(`You've hired the team and there are no more upgrades.`)
     }
+}
+
+function winCondition () {
+    if (player.money >= 1000 && player.tool === tools.length - 1) {
+        alert (`Congrats!!! You have accumulated 1000 dollars. You have won the game`)
+        player.wonGame = true
+    }
+}
+
+alert (`Welcome to the landscaper simulation`)
+
+While (!player.wonGame) {
+    por
 }
